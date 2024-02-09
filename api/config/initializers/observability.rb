e22db94854ebd87447b892522c103e0d5043aeda
Borna Aristan <borna.aristan@productive.io>
2024-02-09 11:49:59 +0100

    add trace_id and span_id to api

--- api/config/initializers/observability.rb
+++ api/config/initializers/observability.rb
@@ -10,6 +10,10 @@ Rails.application.configure do
   # Logging -- single line logs, json format
   config.lograge.enabled = true
   config.lograge.formatter = Lograge::Formatters::Json.new
+  config.lograge.custom_options = lambda do |event|
+    span = OpenTelemetry::Trace.current_span
+    { "trace_id" => span.context.hex_trace_id, "span_id" => span.context.hex_span_id }
+  end
   config.lograge.logger = ActiveSupport::Logger.new($stdout)
 
   # Metrics -- Prometheus
